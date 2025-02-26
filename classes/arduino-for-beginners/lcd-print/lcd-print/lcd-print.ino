b861ec7acc91afbc8e82c81e6d7b702bb5689e43
kyriosaa <kyriosaaph@gmail.com>
2025-02-27 01:58:47 +0700

    Add lcd-print

---
+++ classes/arduino-for-beginners/lcd-print/lcd-print/lcd-print.ino
@@ -0,0 +1,32 @@
+// #include <LiquidCrystal.h>
+
+// for lcd screens that dont have I2C
+// #define LCD_E_PIN A4
+// #define LCD_RS_PIN A5
+// #define LCD_D4_PIN 6
+// #define LCD_D5_PIN 7
+// #define LCD_D6_PIN 8
+// #define LCD_D7_PIN 9
+
+// -----------------------------------------------------------------
+
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+#define LCD_ADDRESS 0x27
+
+LiquidCrystal_I2C lcd(LCD_ADDRESS, 16, 2);
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(4, 0);
+  lcd.print("Hello World!");
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
